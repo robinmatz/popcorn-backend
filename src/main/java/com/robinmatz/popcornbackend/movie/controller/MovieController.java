ece34b28aa8a8bc3031c0ab6e201d067fded32c6
Robin Matz <robin@robinmatz.com>
2025-11-19 10:53:13 +0200

    Create Swagger documentation

---
+++ src/main/java/com/robinmatz/popcornbackend/movie/controller/MovieController.java
@@ -0,0 +1,18 @@
+package com.robinmatz.popcornbackend.movie.controller;
+
+import com.robinmatz.popcornbackend.movie.MovieDetailsDto;
+import com.robinmatz.popcornbackend.movie.MovieDto;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+public interface MovieController {
+    @GetMapping(value = "/api/movies", produces = MediaType.APPLICATION_JSON_VALUE)
+    List<MovieDto> getMovies(@RequestParam String name);
+
+    @GetMapping(value = "/api/movies/{imdbID}/details", produces = MediaType.APPLICATION_JSON_VALUE)
+    MovieDetailsDto getMovieDetails(@PathVariable("imdbID") String imbdId);
+}
---
+++ src/main/java/com/robinmatz/popcornbackend/movie/controller/MovieControllerImpl.java
@@ -0,0 +1,32 @@
+package com.robinmatz.popcornbackend.movie.controller;
+
+import com.robinmatz.popcornbackend.movie.MovieDetailsDto;
+import com.robinmatz.popcornbackend.movie.MovieDto;
+import com.robinmatz.popcornbackend.movie.MovieService;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class MovieControllerImpl implements MovieController {
+
+    private final MovieService movieService;
+
+    public MovieControllerImpl(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @Override
+    @ResponseBody
+    public List<MovieDto> getMovies(String name) {
+        return movieService.getMovies(name);
+    }
+
+    @Override
+    @ResponseBody
+    public MovieDetailsDto getMovieDetails(String imbdId) {
+        return movieService.getMovieDetails(imbdId);
+    }
+
+}
