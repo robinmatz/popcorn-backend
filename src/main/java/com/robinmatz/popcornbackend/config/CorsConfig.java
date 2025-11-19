73482b8395229e102ba3add438c96fbd38312ba2
Robin Matz <robin@robinmatz.com>
2025-11-19 21:32:24 +0200

    Configure CORS

---
+++ src/main/java/com/robinmatz/popcornbackend/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.robinmatz.popcornbackend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/api/**");
+    }
+}
