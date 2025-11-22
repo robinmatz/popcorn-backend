1813f956065c6f64d21da0d64512477bbfffac95
Robin Matz <robin@robinmatz.com>
2025-11-22 06:24:17 +0200

    Return proper response status for MovieNotFoundException

--- src/main/java/com/robinmatz/popcornbackend/movie/exception/MovieControllerExceptionHandler.java
+++ src/main/java/com/robinmatz/popcornbackend/movie/exception/MovieControllerExceptionHandler.java
@@ -2,8 +2,10 @@ package com.robinmatz.popcornbackend.movie.exception;
 
 import com.robinmatz.popcornbackend.global.ErrorDto;
 import com.robinmatz.popcornbackend.movie.controller.MovieController;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice(basePackageClasses = MovieController.class)
@@ -11,6 +13,7 @@ public class MovieControllerExceptionHandler {
 
   @ExceptionHandler(MovieNotFoundException.class)
   @ResponseBody
+  @ResponseStatus(HttpStatus.NOT_FOUND)
   public ErrorDto handleMovieNotFoundException(MovieNotFoundException ex) {
     return new ErrorDto(ex.getMessage());
   }
